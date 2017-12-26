c31d8372d6f53b744c1ec2e88d0c395118319b2e
HiperHedron <skaven1313>
2017-12-26 16:31:06 +0100

    xlsx & resources folder
    czytanie z XLSX dla dataprovider
    folder dla resources

--- src/main/java/com/entry6/qa/util/TestUtil.java
+++ src/main/java/com/entry6/qa/util/TestUtil.java
@@ -1,10 +1,55 @@
 package com.entry6.qa.util;
 
-public class TestUtil {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import com.entry6.qa.base.TestBase;
+
+public class TestUtil extends TestBase{
 	
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 10;
 	public static long FLUENT_WAIT = 7;
 	public static long FLUENT_POLL = 250;
+	private static String TESTDATA_SHEET_PATH ="jakas sciezka";
+	
+	static Workbook book;
+	static Sheet sheet;
+	
+	
+	public static Object[][] getDataFromXLSX(String sheetName) {
+		FileInputStream file = null;
+		try {
+			file = new FileInputStream(TESTDATA_SHEET_PATH );
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			book = WorkbookFactory.create(file);
+		} catch (InvalidFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		sheet = book.getSheet(sheetName);
+		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+		// System.out.println(sheet.getLastRowNum() + "--------" +
+		// sheet.getRow(0).getLastCellNum());
+		for (int i = 0; i < sheet.getLastRowNum(); i++) {
+			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
+				data[i][k] = sheet.getRow(i + 1).getCell(k).toString(); //zeby nie zaycznac od 1. wiersza bo tam beda naglowki
+				// System.out.println(data[i][k]);
+			}
+		}
+		return data;
+	}
+	
+	
 
 }
